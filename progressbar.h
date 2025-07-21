fc75a5a56b422c5eefb58a6f69cdf671c4521278
RadioGlitch <57309672+Firsttown@users.noreply.github.com>
2025-07-21 09:28:09 +0300

    Create progressbar.h

---
+++ progressbar.h
@@ -0,0 +1,14 @@
+void Draw_ProgressBar(int progress, int total, int bar_width, std::string mm);
+/*
+//пример использования
+int main()
+{
+    int width = 50 // ширина прогрессбара
+    std::string = "коментарий к прогрессбару"
+    for(int i = 0; i < 100; i++)
+    {
+        Draw_ProgressBar(i + 1, 100, "дело движется");
+    }
+    system("Pause");
+}
+*/
